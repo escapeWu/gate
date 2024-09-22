5f451af200aeb99ed6c19f3b4e2dc40951cf0d28
shancw <shancw1996@gmail.com>
2024-09-22 19:06:41 +0800

    update

--- gate-start.sh
+++ gate-start.sh
@@ -4,6 +4,10 @@ docker stop shancw-gate
 
 docker rm -f shancw-gate
 
+docker rmi shancw-gate
+
+npx prisma generate
+
 docker build -t shancw-gate .
 
-docker run -d -p 7779:3000 --name shancw-gate shancw-gate
+docker run -d -p 7779:3000 --restart always --name shancw-gate shancw-gate
